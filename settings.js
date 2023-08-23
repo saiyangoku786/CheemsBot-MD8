ae15bb8ad32c354ea651c7593ebee4cffedcf7b7
saiyangoku786 <127642925+saiyangoku786@users.noreply.github.com>
2023-08-24 03:06:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,17 +21,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
+global.owner = ['919425473765'] //ur owner number
 global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.ownername = "ANIME HUB GOKU" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
+global.botname = "GOKU CHEEMS V8"
+global.ownernumber = '919425473765'
+global.ownername = 'GOKU'
 global.ownerNumber = ["916909137213@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
